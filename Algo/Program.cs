fe98fcc91cfa0d667f67f4187217cb058203ff02
Vincent <vincent120@orange.fr>
2018-05-31 15:36:48 +0200

    exo 1 partie 1

--- Algo/Program.cs
+++ Algo/Program.cs
@@ -8,8 +8,24 @@ namespace Algo
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            
+
         }
+
+        private static int GererMenu(List<string> menu)
+        {    
+            foreach (string ligne in menu)
+            {
+                Console.WriteLine(ligne);
+            }
+
+            int choix;
+            choix = int.Parse(Console.ReadLine());
+            return choix;
+        }
+
     }
 }
